d5920249005d654e9a5da90b02f9267496c164c6
Abdul Mannan <245386525+abdulmannan-556@users.noreply.github.com>
2026-02-08 16:34:53 +0500

    Update navigation.ts

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -1,1 +1,56 @@
+export interface NavigationItem {
+  title: string;
+  href: string;
+}
 
+interface NavigationConfig {
+  main: NavigationItem[];
+  footer: NavigationItem[];
+}
+
+export const navigation: NavigationConfig = {
+  main: [
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Services",
+      href: "/services",
+    },
+    {
+      title: "PSX",
+      href: "/psx",
+    },
+    {
+      title: "Insights",
+      href: "/insights",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ],
+  footer: [
+    {
+      title: "About Us",
+      href: "/about",
+    },
+    {
+      title: "Services",
+      href: "/services",
+    },
+    {
+      title: "Compliance",
+      href: "/compliance",
+    },
+    {
+      title: "Careers",
+      href: "/careers",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ],
+};
