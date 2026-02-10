57a09b472ddfdf699eca26177ea56103ae3036e7
Abdul Mannan <245386525+abdulmannan-556@users.noreply.github.com>
2026-02-10 13:21:56 +0000

    commit changes

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: { 
+    // React Compiler disabled (already decided)
+    // optional: remove if using default
+  },
+}
+
+module.exports = nextConfig
